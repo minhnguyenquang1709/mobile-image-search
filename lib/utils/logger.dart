1a138dfdc16734ddf3bda2a6c8b0f1e94f4f4ae9
MinhNQ-PC <minhnguyenquang2004vn@gmail.com>
2025-12-22 01:38:09 +0700

    minor

---
+++ lib/utils/logger.dart
@@ -0,0 +1,24 @@
+import 'dart:developer';
+
+class Logger {
+  final String name;
+  bool mute = false;
+
+  // _cache is library-private, thanks to
+  // the _ in front of its name.
+  static final Map<String, Logger> _cache = <String, Logger>{};
+
+  factory Logger(String name) {
+    return _cache.putIfAbsent(name, () => Logger._internal(name));
+  }
+
+  factory Logger.fromJson(Map<String, Object> json) {
+    return Logger(json['name'].toString());
+  }
+
+  Logger._internal(this.name);
+
+  void printLog(String msg) {
+    if (!mute) log(msg);
+  }
+}
