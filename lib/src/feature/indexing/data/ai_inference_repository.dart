b93f4dcbff268d9bec953df2b58d2b1272ffdad1
minhnq-pc <minhnguyenquang2004vn@gmail.com>
2026-07-12 10:23:02 +0700

    - refactor: comment out unused code

--- lib/src/feature/indexing/data/ai_inference_repository.dart
+++ lib/src/feature/indexing/data/ai_inference_repository.dart
@@ -4,20 +4,20 @@ import 'dart:typed_data';
 import 'package:mobile_image_search/src/data/services/onnx_service.dart';
 import 'package:mobile_image_search/src/feature/indexing/domain/ai_inference_interface.dart';
 
-class OnnxInferenceRepository implements IAiInferenceRepository {
-  OnnxInferenceRepository(this._onnxDataSource);
+// class OnnxInferenceRepository implements IAiInferenceRepository {
+//   OnnxInferenceRepository(this._onnxDataSource);
 
-  final OnnxService _onnxDataSource;
+//   final OnnxService _onnxDataSource;
 
-  Future<void> init() async {}
+//   Future<void> init() async {}
 
-  @override
-  Future<Float32List> encodeImage(File imageFile) async {
-    throw UnimplementedError("Image encoding not implemented yet");
-  }
+//   @override
+//   Future<Float32List> encodeImage(File imageFile) async {
+//     throw UnimplementedError("Image encoding not implemented yet");
+//   }
 
-  @override
-  Future<Float32List> encodeText(String text) async {
-    return await _onnxDataSource.encodeText(text);
-  }
-}
+//   @override
+//   Future<Float32List> encodeText(String text) async {
+//     return await _onnxDataSource.encodeText(text);
+//   }
+// }
--- lib/src/feature/indexing/domain/ai_inference_interface.dart
+++ lib/src/feature/indexing/domain/ai_inference_interface.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'dart:typed_data';
 
-abstract class IAiInferenceRepository {
-  Future<Float32List> encodeImage(File imageFile);
-  Future<Float32List> encodeText(String text);
-}
+// abstract class IAiInferenceRepository {
+//   Future<Float32List> encodeImage(File imageFile);
+//   Future<Float32List> encodeText(String text);
+// }
